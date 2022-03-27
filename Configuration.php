5e3e4cce3396cc3a784c4db3fb41911992572f10
stefant22 <stefantovilovic22@gmail.com>
2022-03-27 09:55:39 +0200

    Update Configuration.php

--- Configuration.php
+++ Configuration.php
@@ -3,7 +3,7 @@ final class Configuration{
     const BASE = 'https://magacin/';
     const DATABASE_HOST='localhost';
     const DATABASE_USER='root';
-    const DATABASE_PASS='aeZakmI1.';
+    const DATABASE_PASS='XXXXXXXX';
     const DATABASE_NAME='magacin';
 
 }
